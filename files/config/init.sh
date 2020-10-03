61e383b7aee6e18cbc9d5c909c5ade2214e189ef
fo40225 <fo40225@hotmail.com>
2020-10-03 09:44:04 +0800

    sudo docker run --privileged -it -p 8082:8082 -p 50030:50030 -p 50060:50060 -v ~/cloudgene-apps-docker:/opt/cloudgene/applications/cloudgene-apps-docker seppinho/cloudgene-docker start-cloudgene.sh

--- files/config/init.sh
+++ files/config/init.sh
@@ -1,2 +1,4 @@
 
 sudo cp /opt/cloudgene/applications/cloudgene-apps-docker/files/config/settings.yaml /opt/cloudgene/config/settings.yaml
+sudo -u cloudgene hadoop fs -mkdir /user/cloudgene/jbwa-data/
+sudo -u cloudgene hadoop fs -put mtdna-server/*.tar.gz /user/cloudgene/jbwa-data/
